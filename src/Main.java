6acb1313ad6133129ecdbf0655bb41ca12f6b8f1
Serg <ksyssau@ya.ru>
2023-07-16 20:27:30 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        System.out.println(nums);
+        task1(nums);
+    }
+
+    public static void task1(List<Integer> nums) {
+
+
         for (Integer num : nums) {
             if (num % 2 != 0) {
                 System.out.println(num);
