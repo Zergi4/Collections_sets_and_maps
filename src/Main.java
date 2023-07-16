480896efbb4f3866441be26153e5d836925feb1a
Serg <ksyssau@ya.ru>
2023-07-16 20:47:45 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(1,1, 2, 3, 4, 4, 5, 5, 6, 7));
         task1(nums);
         task2(nums);
     }
@@ -19,7 +19,8 @@ public class Main {
     }
 
     public static void task2(List<Integer> nums) {
-        Set<Integer> result = new HashSet<>();
+
+        Set<Integer> result = new TreeSet<>();
         for (Integer num : nums) {
             if (num % 2 == 0) {
                 result.add(num);
